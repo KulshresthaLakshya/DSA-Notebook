bd925cd11626ddb45219d04d685f8716a02faae1
Lakshya Kulshrestha <kulshresthalakshya2006@gmail.com>
2026-02-01 23:28:06 +0530

    Implement array insertion in C program

--- 100 Days of DSA/Day1_01.c
+++ 100 Days of DSA/Day1_01.c
@@ -1,1 +1,34 @@
-.
+#include <stdio.h>
+int main()
+{
+  int N;
+  printf("Make an input for size of Array:\n");
+  scanf("%d", &N);
+
+  int a[N+1];
+  printf("Fill in your Array\n");
+  for(int i=0; i<N; i++)
+  {
+    printf("Position %d: \n", i+1);
+    scanf("%d", &a[i]);
+  }
+
+  int pos, new;
+  printf("Enter the position and New value\n");
+  scanf("%d\n%d", &pos, &new);
+
+  //[][][][][]+[]
+
+  for(int j=N; j>=pos; j--)
+  {
+    a[j] = a[j-1];
+  }
+  a[pos-1]=new;
+
+  printf("Updated Array:\n");
+  for(int i=0; i<=N; i++)
+  {
+    printf("%d\n", a[i]);
+  }
+  return 0;
+}
