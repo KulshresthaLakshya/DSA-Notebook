ffba1e2abe76d5f4f42c53f4c9afcdb8a201189c
Lakshya Kulshrestha <kulshresthalakshya2006@gmail.com>
2026-02-22 18:12:42 +0000

    Arrays

--- DSA+C_Lab/Program2_3.c
+++ DSA+C_Lab/Program2_3.c
@@ -1,48 +1,43 @@
 /*
 2.1) Inputing & Printing 2D Array
 2.2) Multiplication of 2, 2D Arrays
-2.3) Printing Row & column of 2D Matrix
+2.3) Printing Row & column SUM of 2D Matrix
 2.4) Create Structure Student with following: SAP iD, Name, CGPA & print SAP ID of people with CGPA>=7
 */
-
 #include <stdio.h>
-int main()
-{
-    int N,M;
+int main() {
+    int N, M;
     printf("Provide the Rows of the Array: ");
     scanf("%d", &N);
     printf("Provide the Columns of the Array: ");
     scanf("%d", &M);
 
     int a[N][M];
-    
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<M; j++)
-        {
-            printf("Provide the element for (i,j)=%d,%d: ", i,j);
+
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<M; j++) {
+            printf("Element [%d][%d]: ", i, j);
             scanf("%d", &a[i][j]);
         }
     }
 
-    printf("\n");
-    for(int i=0; i<N; i++)
-    {
-        printf("Row %d is: ", i);
-        for(int j=0; j<M; j++)
-        {
-            printf("[%d]", a[i][j]);
+    printf("\n--- Row Sums ---\n");
+    for(int i=0; i<N; i++) {
+        int rowSum = 0;
+        for(int j=0; j<M; j++) {
+            rowSum += a[i][j];
         }
-        printf("\n");
+        printf("Sum of Row %d: %d\n", i, rowSum);
     }
-    for(int i=0; i<N; i++)
-    {
-        printf("Column %d is: ", i);
-        for(int j=0; j<M; j++)
-        {
-            printf("[%d]", a[j][i]);
+
+    printf("\n--- Column Sums ---\n");
+    for(int j=0; j<M; j++) {
+        int colSum = 0;
+        for(int i=0; i<N; i++) {
+            colSum += a[i][j];
         }
-        printf("\n");
+        printf("Sum of Column %d: %d\n", j, colSum);
     }
+
     return 0;
 }
--- DSA+C_Lab/Program2_4.c
+++ DSA+C_Lab/Program2_4.c
@@ -1,7 +1,39 @@
-/*
-2.1) Inputing & Printing 2D Array
-2.2) Multiplication of 2, 2D Arrays
-2.3) Printing Row & column of 2D Matrix
-2.4) Create Structure Student with following: SAP iD, Name, CGPA & print SAP ID of people with CGPA>=7
-*/
+#include <stdio.h>
 
+struct Student {
+    int sapID;
+    char name[50];
+    float cgpa;
+};
+
+int main() {
+    int n;
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    struct Student s[n]; 
+    for(int i = 0; i < n; i++) {
+        printf("\nEnter details for Student %d\n", i + 1);
+        printf("SAP ID: ");
+        scanf("%d", &s[i].sapID);
+        printf("Name: ");
+        scanf(" %[^\n]s", s[i].name); 
+        printf("CGPA: ");
+        scanf("%f", &s[i].cgpa);
+    }
+
+    printf("\n--- SAP IDs of Students with CGPA >= 7 ---\n");
+    int found = 0;
+    for(int i = 0; i < n; i++) {
+        if(s[i].cgpa >= 7.0) {
+            printf("SAP ID: %d (Name: %s)\n", s[i].sapID, s[i].name);
+            found = 1;
+        }
+    }
+
+    if(!found) {
+        printf("No students found with CGPA >= 7.\n");
+    }
+
+    return 0;
+}
