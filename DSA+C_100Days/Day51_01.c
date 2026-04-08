c6d5ea656e14cbf738180d4c41a3e6ed4837705f
Lakshya Kulshrestha <kulshresthalakshya2006@gmail.com>
2026-04-08 23:27:41 +0530

    Implement BST with LCA function

---
+++ DSA+C_100Days/Day51_01.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct Node* insert(struct Node* node, int data) {
+    if (node == NULL) return newNode(data);
+    if (data < node->data)
+        node->left = insert(node->left, data);
+    else
+        node->right = insert(node->right, data);
+    return node;
+}
+
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    while (root != NULL) {
+        if (root->data > n1 && root->data > n2)
+            root = root->left;
+        else if (root->data < n1 && root->data < n2)
+            root = root->right;
+        else
+            break;
+    }
+    return root;
+}
+
+int main() {
+    int n, val, n1, n2;
+    struct Node* root = NULL;
+
+    if (scanf("%d", &n) != 1) return 0;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* lca = findLCA(root, n1, n2);
+    
+    if (lca != NULL) {
+        printf("%d\n", lca->data);
+    }
+
+    return 0;
+}
