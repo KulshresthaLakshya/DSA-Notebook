8844d775fb987abe47b3d713ffd891e01b9a239c
Lakshya Kulshrestha <kulshresthalakshya2006@gmail.com>
2026-04-30 22:25:04 +0530

    Add Floyd-Warshall algorithm implementation
    Implement Floyd-Warshall algorithm for shortest paths.

---
+++ DSA+C_100Days/Day80_01.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+#define INF 1000000
+
+void floydWarshall(int n, int dist[n][n]) {
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 0;
+
+    int dist[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < j < n; j++) {
+            int val;
+            scanf("%d", &val);
+            if (val == -1 && i != j) {
+                dist[i][j] = INF;
+            } else {
+                dist[i][j] = val;
+            }
+        }
+    }
+
+    floydWarshall(n, dist);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF) {
+                printf("-1");
+            } else {
+                printf("%d", dist[i][j]);
+            }
+            printf(j == n - 1 ? "" : " ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
