c2ef70f0557d9c12e2870d6b3d519ccb2b98681b
Lakshya Kulshrestha <kulshresthalakshya2006@gmail.com>
2026-05-01 20:14:37 +0530

    Add merge sort implementation
    Implement merge sort algorithm to sort an array of integers.

---
+++ DSA+C_100Days/Day85_01.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void merge_sort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 0;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        if (scanf("%d", &arr[i]) != 1) break;
+    }
+
+    merge_sort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d%s", arr[i], (i == n - 1) ? "" : " ");
+    }
+    printf("\n");
+
+    return 0;
+}
